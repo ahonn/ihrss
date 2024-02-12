cc737dda6ac140649a9c1f90b0f61ba6bc03264a
Yuexun Jiang <yuexunjiang@gmail.com>
2024-02-12 11:19:27 +1100

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -7,9 +7,9 @@ import withCache from './cache';
 const router = Router();
 
 router.get('/', () => Response.redirect('https://github.com/ahonn/ihrss', 301));
-router.get('/:type', withCache(storiesRouter, 60 * 10));
-router.get('/group/:name', withCache(groupRouter, 60 * 10));
-router.get('/top/:type', withCache(topRouter, 60 * 10));
+router.get('/:type', withCache(storiesRouter, 60 * 60 * 24));
+router.get('/group/:name', withCache(groupRouter, 60 * 60 * 24));
+router.get('/top/:type', withCache(topRouter, 60 * 60 * 24));
 
 // 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }));
